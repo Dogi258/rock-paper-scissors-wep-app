7f3cd653622905b962d2eede3ef2fb97922ccb75
Daniel <mtz.daniel21@gmail.com>
2021-10-15 04:06:38 -0500

    finished logic for rock paper scissors game

--- script.js
+++ script.js
@@ -12,50 +12,66 @@ function computerPlay() {
   }
 }
 
-// Play a round of rock paper scissors
-function playRound(playerSelection, computerSelection) {
+/*  Returns and array where the first element contains a message string for the 
+    winner of the round and the second element contains the score value that will 
+    be added to the running score.*/
+function getWinnerArray(playerSelection, computerSelection) {
   // Make player selection case insensitive
   playerSelection = playerSelection.toLowerCase();
 
-  // Returns the winner
+  // Returns the winner array
   if (playerSelection == "rock" && computerSelection == "paper") {
-    return "You lose! Paper beats Rock";
+    return ["You lose! Paper beats Rock", -1];
   } else if (playerSelection == "rock" && computerSelection == "scissors") {
-    return "You win! Rock beats Scissors";
+    return ["You win! Rock beats Scissors", 1];
   } else if (playerSelection == "paper" && computerSelection == "scissors") {
-    return "You lose! Scissors beats Paper";
+    return ["You lose! Scissors beats Paper", -1];
   } else if (playerSelection == "paper" && computerSelection == "rock") {
-    return "You win! Paper beats Rock";
+    return ["You win! Paper beats Rock", 1];
   } else if (playerSelection == "scissors" && computerSelection == "rock") {
-    return "You lose! Rock beats Scissors";
+    return ["You lose! Rock beats Scissors", -1];
   } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    return "You win! Scissors beats Paper";
+    return ["You win! Scissors beats Paper", 1];
   } else {
-    return "Tie!";
+    return ["Tie!", 0];
   }
 }
 
-function game() {
-  let score = 0;
-  const numOfRounds = 5;
-  for (let i = 0; i < numOfRounds; i++) {
-    // Get user input
-    let playerSelection = window.prompt("Rock, paper, or scissors?");
-    // Get computer selection
-    let computerSelection = computerPlay();
-    // Get round message
-    let roundMsg = playRound(playerSelection, computerSelection);
-    console.log(roundMsg);
+// Update the UI based on who won the round
+function updateUI(winnerArray) {
+  // Get message element
+  const message = document.querySelector(".container-winner h3");
+  console.log(message);
 
-    // Add to the final score if player won
-    if (roundMsg.includes("win")) {
-      score++;
-    }
-  }
-  // Show wiinner
-  if (score < 3) {
-    console.log("Player loses game! " + score + "/" + numOfRounds);
-  } else console.log("Player wins game! " + score + "/" + numOfRounds);
+  // Update message
+  message.textContent = winnerArray[0];
+
+  // Update score
+  score += winnerArray[1];
+
+  // Update Score UI
+  const scoreElement = document.querySelector(".container-score h4");
+  scoreElement.textContent = score;
 }
 
-//game();
+function playRound(e) {
+  let winnerArray;
+
+  if (e.target.id == "btn-rock") {
+    winnerArray = getWinnerArray("rock", computerPlay());
+  } else if (e.target.id == "btn-paper") {
+    winnerArray = getWinnerArray("paper", computerPlay());
+  } else {
+    winnerArray = getWinnerArray("scissors", computerPlay());
+  }
+
+  updateUI(winnerArray);
+}
+// Game score
+let score = 0;
+
+// Get all three buttons
+const gameButtons = document.querySelectorAll(".button");
+
+// Add event listeners to play a round of the game
+gameButtons.forEach((button) => button.addEventListener("click", playRound));
